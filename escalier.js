8456ebab8236fe27f8576a13e74073d72f05c116
Lionel Limol <limol.lionel@gmail.com>
2021-06-27 15:14:27 +0200

    Initial commit with of my js
    Here I put let say the first version of my file in my repository

---
+++ escalier.js
@@ -0,0 +1,18 @@
+escalier = (step) => {
+  let str = [];
+
+  for (let i = 1; i <= step; i++) {
+    for (let j = 1; j <= step; j++) {
+      if (j > step - i) {
+        str += "#";
+      } else {
+        str += " ";
+      }
+    }
+    str += "\n";
+  }
+  return str;
+};
+
+let marche = 20;
+console.log(escalier(marche));
